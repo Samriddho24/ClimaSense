307864f7349905ac5d2fb675b391f809781d08a4
Samriddho Kar <samriddhokar8@gmail.com>
2026-07-02 23:42:52 +0530

    Implemented city input

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css";
 
 function App() {
+  const[city, setCity] = useState(" ");
   return (
     <div className='container'>
-        <h1>Hello ClimaSense</h1>
+        <h1>ClimaSense</h1>
         <p>Know the weather before you step outside.</p>
-        <input type="text" placeholder='Enter city Name'/>
-        <button>Search</button>
+        <input 
+          type="text"
+          placeholder='Enter city Name'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
     </div>
   )
+  function handleSearch(){
+    console.log(city);
+  }
 }
 
 export default App
